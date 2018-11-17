238baa924d5c5cafd9cbf6fe94230d0033a8a040
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-11-17 22:50:01 +0200

    Add files via upload

---
+++ 1411_03.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1411_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+           
+
+            for (int j = 1; j <= x; j++)
+            {
+
+
+                for (int i = 1; i <= j; i++)
+                {
+
+                    Console.Write(i);
+
+                }
+                Console.WriteLine();
+            }
+            for (int j = x-1; j >= 0; j--)
+            {
+                for (int i = 1; i <= j; i++)
+                {
+
+                    Console.Write(i);
+
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
