2bb2c76d604ed6e9672652064479d6ba77fcf5e0
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-11-17 22:53:24 +0200

    Add files via upload

---
+++ 1411_01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1411_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rishoni();
+
+        }
+
+        private static void rishoni()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int m = 2;
+            while (a % m != 0 && m < a)
+            {
+                m++;
+            }
+            if (a == m)
+            {
+                Console.WriteLine("rishoni");
+                
+            }
+            else
+                Console.WriteLine("try again");
+                rishoni();
+        }
+    }
+}
