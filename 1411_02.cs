9961fc5e9ec6dbbf6ebaef57dc000013924ca2c8
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-11-17 22:51:18 +0200

    Add files via upload

---
+++ 1411_02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1411_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            int mea = t / 100;
+            int shloshim = (t - mea * 100) / 30;
+            int bait = (t - mea * 100 - shloshim * 30);
+            Console.WriteLine(mea);
+            Console.WriteLine(shloshim);
+            Console.WriteLine(bait);
+        }
+    }
+}
